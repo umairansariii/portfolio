a28e6118e2cfd7502850790cc2affeeeed9fc83f
umairansari <umairansari.work@gmail.com>
2023-07-06 18:40:59 +0500

    Added process section

--- src/app.jsx
+++ src/app.jsx
@@ -8,8 +8,8 @@ export default function App() {
         <div className="site">
             <Header/>
             <DesignerSection/>
+            <ProcessSection/>
             <DeveloperSection/>
-            {/* <ProcessSection/> */}
         </div>
     )
 }
--- src/components/sections/process.jsx
+++ src/components/sections/process.jsx
@@ -4,14 +4,13 @@ export default function ProcessSection() {
     return (
         <section className="process">
             <p className="title-big">The working path</p>
-            <p className="title-sub">How your project processed</p>
+            <p className="title-sub">You&apos;ve the <u>great idea</u>, we have the <u>right plan</u> for you.<br/>Great ideas often thrive when they are nurtured within a supportive <br/>and collaborative environment.</p>
             <div className="process-path">
-                <div className="process-path-arrow-i"></div>
+                <div className="process-path-arrow"></div>
                 <div className="process-path-i">Requirement Analysis</div>
-                <div className="process-path-ii">Initial Prototyping</div>
+                <div className="process-path-ii">Brainstorming</div>
                 <div className="process-path-iii">Implementation</div>
                 <div className="process-path-iv">Client Feedback</div>
-                <div className="process-path-arrow-iii"></div>
             </div>
         </section>
     )
