7291b8bd53d8d9e30b03b7392f5ae75cd4beb815
umairansari <umairansari.work@gmail.com>
2023-07-31 21:45:43 +0500

    Added quote section

---
+++ src/components/sections/quote.jsx
@@ -0,0 +1,10 @@
+import "./scss/quote.scss";
+
+export default function QuoteSection() {
+    return (
+        <section className="quote">
+            <p className="title-big">You&apos;ve the idea</p>
+            <p className="title-sub">Get a <u>personalized</u> quotation for your next project, collaborate and create something <u>extraordinary</u> together!</p>
+        </section>
+    )
+}
